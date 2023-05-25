ac059627497cab83da0fd30f39e2eb4a1879ab9a
ginogiorgi <ginorubengiorgi@gmail.com>
2023-05-25 18:34:16 -0300

    Clase 19 del Curso de Asincronismo con JavaScript de Platzi

---
+++ src/generator/index.js
@@ -0,0 +1,25 @@
+function* gen() {
+    yield 1;
+    yield 2;
+    yield 3;
+};
+
+const generator = gen();
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next().value);
+
+function* iterate(array) {
+    for (let value of array) {
+        yield value;
+    };
+};
+
+const it = iterate(["Gino", "Tu hermana", "Lol", "Dou", "Xd"]);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().value);
